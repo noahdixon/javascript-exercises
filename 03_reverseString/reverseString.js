c4ff639fdf348ebc31cea509648a91770c008f63
Noah Dixon <114971241+noahdixon@users.noreply.github.com>
2023-01-14 00:00:54 -0500

    Complete exercises 1, 2, and 3.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
 
+    if(typeof inputString !== 'string') {
+        return 'ERROR'
+    }
+
+    let reverse = '';
+    for (let i = inputString.length; i > 0; i--) {
+        reverse += inputString.charAt(i-1);
+    }
+
+    return reverse;
 };
 
 // Do not edit below this line
