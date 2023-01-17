49c749b3f93c0f4cf150ae719a348160ea695c7a
Noah Dixon <114971241+noahdixon@users.noreply.github.com>
2023-01-16 22:00:11 -0500

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
 const removeFromArray = function() {
+    const inputArray = arguments[0];
+    const removeSet = new Set();
 
+    for (let i = 1; i < arguments.length; i++) {
+        // if (typeof arguments[i] !== typeof inputArray[0]) {
+        //     continue;
+        // }
+        removeSet.add(arguments[i]);
+    }
+
+    const inputLength = inputArray.length;
+
+    for (let i = 0; i < inputLength; i++) {
+        if(removeSet.has(inputArray[i])) {
+            inputArray.splice(i, 1);
+            i--;
+        }
+    }
+
+    return inputArray;
 };
 
 // Do not edit below this line
