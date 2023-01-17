7dc674d5f09b5904f6124257687d60f3bb127b93
Noah Dixon <114971241+noahdixon@users.noreply.github.com>
2023-01-16 23:02:31 -0500

    complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+
+    if(numOne < 0 || numTwo < 0 || typeof numOne !== 'number' || typeof numTwo !== 'number') {
+        return "ERROR";
+    }
+
+    let largerNum = (numOne >= numTwo) ? numOne : numTwo;
+    let smallerNum = (numOne <= numTwo) ? numOne : numTwo;
+    console.log(largerNum);
+    console.log(smallerNum);
+    let sum = 0;
+    
+    for (let i = smallerNum; i <= largerNum; i++) {
+        sum += i;
+    }
+
+    return sum;
 
 };
 
